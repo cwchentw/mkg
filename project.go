1fd94245b781d1165c6bb2c6320a565035b1caf9
Michael Chen <cwchen.tw@outlook.com>
2018-05-22 23:55:54 +0000

    Add support for gitignore

--- project.go
+++ project.go
@@ -23,6 +23,7 @@ func CreateProject(pr *ParsingResult) {
 
 	createLicense(pr)
 	createREADME(pr)
+	createGitignore(pr)
 }
 
 func createLicense(pr *ParsingResult) {
@@ -66,6 +67,32 @@ func createREADME(pr *ParsingResult) {
 	_, err = file.WriteString(
 		fmt.Sprintf(template_readme,
 			pr.Prog(), pr.Brief(), fmt.Sprint(now.Year()), pr.Author()))
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+func createGitignore(pr *ParsingResult) {
+	path := filepath.Join(pr.Path(), ".gitignore")
+	file, err := os.Create(path)
+	defer file.Close()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	var ignore string
+	switch pr.Lang() {
+	case LANG_C:
+		ignore = gitignore_c
+	case LANG_CPP:
+		ignore = gitignore_cpp
+	default:
+		panic("Unknown language")
+	}
+
+	_, err = file.WriteString(ignore)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
